7f8a5fe8c4ce8aafc965d375a53549612659d176
QuantumPlank <QuantumPlank@outlook.com>
2024-05-19 16:55:19 -0600

    Add formatting function

--- C/src/bitstrings.c
+++ C/src/bitstrings.c
@@ -1,5 +1,12 @@
 #include"bitstrings.h"
 
 void formatBitString(unsigned int bitStringSize, char* bitString){
-
+    for(unsigned int i=0; i<bitStringSize; i++){
+        if(bitString[i] == '0'){
+            bitString[i] = '0';
+        }
+        else {
+            bitString[i] = '1';
+        }
+    }
 }
--- C/test/test_bitstrings.c
+++ C/test/test_bitstrings.c
@@ -1,8 +1,20 @@
 #include"bitstrings.h"
 #include"unity.h"
+#include<string.h>
+
+#define testBitStringSize 8
+
+char bitStringA[testBitStringSize];
+char bitStringB[testBitStringSize];
+char bitStringC[testBitStringSize];
+char bitStringD[testBitStringSize];
 
 void setUp(void) 
 {
+    strcpy(bitStringA, "abcdefgh");
+    strcpy(bitStringB, "00000000");
+    strcpy(bitStringC, "11111111");
+    strcpy(bitStringD, "10101010");
 }
 
 void tearDown(void)
@@ -11,7 +23,9 @@ void tearDown(void)
 
 void test_formatBitString(void)
 {
-  
+    formatBitString(testBitStringSize, bitStringA);
+    TEST_ASSERT_EQUAL_STRING("11111111", bitStringA);
+
 }
 
 int main(void) {
