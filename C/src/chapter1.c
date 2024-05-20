3a23db7032ed2efe8f3cbad0170082a7d3465b96
QuantumPlank <QuantumPlank@outlook.com>
2024-05-19 20:45:40 -0600

    Reorganize C project structure

--- C/src/chapter1.c
+++ C/src/chapter1.c
@@ -4,7 +4,13 @@
 #include<stdio.h>
 #include<ctype.h>
 
-
+/**
+ * @brief Chapter: 1. Computer Projects: 1.
+ * Given the truth values of the propositions p and q, find the truth values of the conjunction, disjunction, exclusive
+ * or, conditional statement, and biconditional of these propositions.
+ * 
+ * @return int 
+ */
 int ch1_cp1(void){
     bool p = false;
     bool q = false;
@@ -46,6 +52,12 @@ int ch1_cp1(void){
     return 0;
 }
 
+/**
+ * @brief Chapter: 1. Computer Projects: 2.
+ * Given two bit strings of length n, find the bitwise AND, bitwise OR, and bitwise XOR of these strings.
+ * 
+ * @return int 
+ */
 int ch1_cp2(void){
     const unsigned int bitStringSize = 32; 
     char bitStringA[bitStringSize];
@@ -63,3 +75,43 @@ int ch1_cp2(void){
     printf("%s\n", bitStringB);
     return 0;
 }
+
+
+/**
+ * @brief Chapter: 1. Computer Projects: 3.
+ * Give a compound proposition, determine whether it is satisfiable by checking its truth value for all positive
+ * assignments of truth values to its propositional variables.
+ *
+ * @return int 
+ */
+int ch1_cp3(void){
+    return 0;
+} 
+
+/**
+ * @brief Chapter: 1. Computer Projects: 4.
+ * Given the truth values of the propositions p and q in fuzzy logic, find the truth value of the disjunction and the
+ * conjunction of p and q (see Exercises 46 and 47 of Section 1.1).
+ *
+ * @return int 
+ */
+int ch1_cp4(void){
+    return 0;
+}
+
+/**
+ * @brief Chapter: 1. Computer Projects: 5.
+ * Given positive integers m and n, interactively play the game of Chomp.
+ *
+ * @return int 
+ */
+int ch1_cp5(void);
+
+/**
+ * @brief Chapter: 1. Computer Projects: 6.
+ * Given a portion of a checkerboard, look for tilings of this checkerboard with various types of polyominoes, including
+ * dominoes, the two types of triominoes, and larger polyominoes.
+ *
+ * @return int 
+ */
+int ch1_cp6(void);
--- C/src/chapter1.h
+++ C/src/chapter1.h
@@ -5,4 +5,12 @@ int ch1_cp1(void);
 
 int ch1_cp2(void);
 
+int ch1_cp3(void);
+
+int ch1_cp4(void);
+
+int ch1_cp5(void);
+
+int ch1_cp6(void);
+
 #endif
